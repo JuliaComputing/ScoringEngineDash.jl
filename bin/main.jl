4eca3e34726b7d121c4a93d342c306f66f60eec7
jeremie.db <jeremie.db@evovest.com>
2022-01-14 14:37:41 -0500

    up

--- bin/main.jl
+++ bin/main.jl
@@ -4,12 +4,12 @@ using CSV, JSON3, JSONTables, DataFrames
 using Statistics: mean, std
 using StatsBase: sample
 using ShapML
+using Loess
 
-df_tot = CSV.read(joinpath(@__DIR__, "data", "training_data.csv"), DataFrame)
+df_tot = CSV.read(joinpath(@__DIR__, "../data", "training_data.csv"), DataFrame)
 
-df = df_tot[1:1000, :];
-body = JSON3.write(arraytable(df));
-JSON3.read(body) |> jsontable |> DataFrame;
+sample_size = 20
+features = ["pol_no_claims_discount", "pol_coverage", "pol_duration", "pol_sit_duration", "vh_value", "vh_weight", "vh_age", "population", "town_surface_area", "drv_sex1", "drv_age1", "pol_pay_freq"]
 
 function get_scores(df::DataFrame)
     body = JSON3.write(arraytable(df))
@@ -27,43 +27,31 @@ function add_scores!(df::DataFrame)
     return nothing
 end
 
-
-function pred_shap_flux(model, df)
+function pred_shap(model, df)
     pred = get_scores(df::DataFrame)
-    pred_df = DataFrame(score = pred[:flux])
+    pred_df = DataFrame(score = pred[model])
     return pred_df
 end
 
-sample_size = 30
-explain, reference = copy(df[1:10, :]), copy(df[1:10, :])
-
-features = ["pol_no_claims_discount", "pol_coverage", "pol_duration", "pol_sit_duration", "vh_value", "vh_weight", "vh_age", "population", "town_surface_area", "drv_sex1", "drv_age1", "pol_pay_freq"]
-@time data_shap = ShapML.shap(
-    explain = explain,
-    reference = reference,
-    target_features = features,
-    model = "flux",
-    predict_function = pred_shap_flux,
-    sample_size = sample_size,
-    seed = 123
-)
-data_shap[data_shap.feature_name.=="drv_age1", :]
-data_shap[data_shap.feature_name.=="vh_age", :]
-
-function run_shap(df, model = "flux")
+function run_shap(df, model = :flux)
     data_shap = ShapML.shap(
         explain = copy(df),
         reference = copy(df),
         target_features = features,
-        model = "flux",
-        predict_function = pred_shap_flux,
+        model = model,
+        predict_function = pred_shap,
         sample_size = sample_size,
         seed = 123)
     return data_shap
 end
 
-function plot_shap(data_shap, feat)
-    df = data_shap[data_shap.feature_name.=="vh_age", :]
+function plot_shap(data_shap, feat = "vh_age")
+    df = data_shap[data_shap.feature_name.==feat, :]
+    transform!(df, :feature_value => ByRow(x -> convert(Float64, x)) => :feature_value)
+    model = loess(df[:, :feature_value], df[:, :shap_effect], span = 0.5)
+    smooth_x = range(extrema(df[:, :feature_value])...; length = 10)
+    smooth_y = Loess.predict(model, smooth_x)
+    return (df = df, smooth_x = smooth_x, smooth_y = smooth_y)
 end
 
 function get_feat_importance(data_shap)
@@ -73,19 +61,19 @@ function get_feat_importance(data_shap)
     return df
 end
 
-available_indicators = unique(df_tot[!, "pol_coverage"])
 years = unique(df_tot[!, "year"])
 rng = Random.MersenneTwister(123)
 
 app = dash(external_stylesheets = ["https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"])
 
 app.layout = html_div(className = "p-5") do
+    html_h3("Scoring Engine - Model Exploration Dashboard"),
     html_div([
             html_p("Select Feature"),
             html_div(dcc_dropdown(
-                    id = "xaxis-column",
-                    options = [(label = i, value = i) for i in available_indicators],
-                    value = "Fertility rate, total (births per woman)")),
+                id = "xaxis-column",
+                options = [(label = i, value = i) for i in features],
+                value = "vh_age"), className = "col-md-4"),
             html_div(html_button("Update Sample!", id = "btn-1"), className = "py-3")
         ], className = "col-md-12"),
     html_div(
@@ -101,31 +89,40 @@ callback!(
     Output("plot1", "figure"),
     Output("plot2", "figure"),
     Output("plot3", "figure"),
-    Input("xaxis-column", "value"),
     Input("btn-1", "n_clicks"),
-) do xaxis_column_name, btn_n_clicks
+    Input("xaxis-column", "value"),
+) do btn_n_clicks, xaxis_column_name
     ids = sample(rng, 1:nrow(df_tot), 20, replace = false, ordered = true)
     df = df_tot[ids, :]
     add_scores!(df)
-    data_shap = run_shap(df)
-    feat_imp = get_feat_importance(data_shap)
+    data_flux = run_shap(df, :flux)
+    feat_flux = get_feat_importance(data_flux)
+    shap_flux = plot_shap(data_flux, xaxis_column_name)
+
+    data_gbt = run_shap(df, :gbt)
+    feat_gbt = get_feat_importance(data_gbt)
+    shap_gbt = plot_shap(data_gbt, xaxis_column_name)
+
     println("ids: ", ids)
+
     return (Plot(
-            # [scatter(x = rand(100), y = rand(100), mode = "markers", marker = attr(color="darkgreen"))]
-            scatter(x = df[:, :flux], y = df[:, :gbt], mode = "markers", marker = attr(color = "darkgreen", size = 12)),
+            [scatter(x = shap_flux[:df][:, :feature_value], y = shap_flux[:df][:, :shap_effect], mode = "markers", marker = attr(color = "red", opacity = 0.5, size = 12), name="flux"),
+                scatter(x = shap_flux[:smooth_x], y = shap_flux[:smooth_y], mode = "lines", marker = attr(color = "purple", size = 12), name="flux"),
+                scatter(x = shap_gbt[:df][:, :feature_value], y = shap_gbt[:df][:, :shap_effect], mode = "markers", marker = attr(color = "green", opacity = 0.5, size = 12), name="gbt"),
+                scatter(x = shap_gbt[:smooth_x], y = shap_gbt[:smooth_y], mode = "lines", marker = attr(color = "blue", size = 12), name="gbt")],
             Layout(
                 title = "Flux vs GBT predictions",
                 plot_bgcolor = "white",
                 paper_bgcolor = nothing,
                 xaxis = attr(
-                    title = "xaxis title",
+                    title = xaxis_column_name,
                     showgrid = true,
                     gridcolor = "lightgray",
                     showline = false,
                     linecolor = "black",
                     titlefont_color = "black"),
                 yaxis = attr(
-                    title = "yaxis title",
+                    title = "SHAP effect",
                     showgrid = true,
                     gridcolor = "lightgray",
                     showline = false,
@@ -134,9 +131,9 @@ callback!(
             )
         ),
         Plot(
-            bar(y = feat_imp[:, :feature_name], x = feat_imp[:, :shap_effect], orientation = "h", marker = attr(color = "red", opacity = 0.5)),
+            bar(y = feat_flux[:, :feature_name], x = feat_flux[:, :shap_effect], orientation = "h", marker = attr(color = "red", opacity = 0.5)),
             Layout(
-                title = "Flux Feature Importance",
+                title = "Flux geature importance",
                 plot_bgcolor = "white",
                 paper_bgcolor = nothing,
                 xaxis = attr(
@@ -156,23 +153,23 @@ callback!(
             )
         ),
         Plot(
-            scatter(x = df[:, :flux], y = df[:, :gbt], mode = "markers", marker = attr(color = "purple", size = 12)),
+            bar(y = feat_gbt[:, :feature_name], x = feat_gbt[:, :shap_effect], orientation = "h", marker = attr(color = "green", opacity = 0.5)),
             Layout(
-                title = "Flux vs GBT predictions",
+                title = "GBT feature importance",
                 plot_bgcolor = "white",
                 paper_bgcolor = nothing,
                 xaxis = attr(
                     title = "xaxis title",
                     showgrid = true,
                     gridcolor = "lightgray",
-                    showline = false,
+                    showline = true,
                     linecolor = "black",
                     titlefont_color = "black"),
                 yaxis = attr(
                     title = "yaxis title",
                     showgrid = true,
                     gridcolor = "lightgray",
-                    showline = false,
+                    showline = true,
                     linecolor = "black",
                     titlefont_color = "black")
             )
