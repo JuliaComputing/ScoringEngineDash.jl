376ea553e520991227002b990f560967665f39b1
Pradeep Mudlapur <pradeep@juliacomputing.com>
2023-05-02 23:02:11 +0530

    fix: update 🐛

--- bin/main.jl
+++ bin/main.jl
@@ -9,7 +9,7 @@ using Loess
 
 using ScoringEngineDash
 
-pkgpath = split(pathof(ScoringEngineDash), ".jl")[1]
+pkgpath = dirname(split(pathof(ScoringEngineDash), ".jl")[1])
 
 const url_base = "http://localhost:8008/api/v1"
 # const url_base = get(ENV, "SCORING_URL", "$(url_base)/flux")
